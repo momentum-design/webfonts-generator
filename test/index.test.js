e48b2a9abf812f3d0fb4cf4a74e6f21d4a31fcef
Thomas Bassetto <thobasse@cisco.com>
2019-05-15 10:13:11 +0200

    chore: upgrade dependencies

--- test/index.test.js
+++ test/index.test.js
@@ -19,12 +19,24 @@ describe("generateFonts", function() {
     global.console = {
       log: jest.fn()
     };
-    const result = await generateFonts("My Font", "test/icons/*.svg", "test/dist");
+    const result = await generateFonts(
+      "My Font",
+      "test/icons/*.svg",
+      "test/dist"
+    );
     expect(result.fontName).toEqual("My Font");
-    expect(result.fontFiles.svg).toEqual(path.join("test", "dist", "My Font.svg"));
-    expect(result.fontFiles.ttf).toEqual(path.join("test", "dist", "My Font.ttf"));
-    expect(result.fontFiles.woff).toEqual(path.join("test", "dist", "My Font.woff"));
-    expect(result.fontFiles.woff2).toEqual(path.join("test", "dist", "My Font.woff2"));
+    expect(result.fontFiles.svg).toEqual(
+      path.join("test", "dist", "My Font.svg")
+    );
+    expect(result.fontFiles.ttf).toEqual(
+      path.join("test", "dist", "My Font.ttf")
+    );
+    expect(result.fontFiles.woff).toEqual(
+      path.join("test", "dist", "My Font.woff")
+    );
+    expect(result.fontFiles.woff2).toEqual(
+      path.join("test", "dist", "My Font.woff2")
+    );
     expect(result.glyphsData.length).toEqual(5);
     expect(result.glyphsData[0]).toEqual({
       name: "accessibility_16",
